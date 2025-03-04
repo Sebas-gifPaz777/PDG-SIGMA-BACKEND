14f7e05c86b823555f1e1f096b1c05e1f89326e3
Juan D. Lora <juan.lora1@u.icesi.edu.co>
2025-03-04 15:51:30 -0500

    feat:student/assistant crud

---
+++ src/main/java/com/pdg/sigma/controller/StudentController.java
@@ -0,0 +1,37 @@
+package com.pdg.sigma.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pdg.sigma.domain.Student;
+import com.pdg.sigma.service.StudentServiceImpl;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    @Autowired
+    private StudentServiceImpl studentService;
+    
+    @GetMapping("/getA") 
+    public List<Student> getAllStudents() {
+        return studentService.findAll();
+    }
+
+    @GetMapping("/{code}")  
+    public ResponseEntity<Student> getStudentByCode(@PathVariable String code) {
+        Optional<Student> student = studentService.findById(code);
+
+        return student.map(ResponseEntity::ok)
+                      .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
